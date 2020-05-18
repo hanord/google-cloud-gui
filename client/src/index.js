43725649a6c9b6867d5924d54b8a3e4fe111df89
hanord <hanord@gmail.com>
2020-05-18 18:21:36 +0200

    Not needed

--- client/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import 'roboto-fontface/css/roboto/roboto-fontface.css'
-
-import './index.css'
-import App from './App'
-
-ReactDOM.render(<App />, document.getElementById('root'))
